93e25bc06c88aed8184327c43815c67765ba0319
eugene <eugenefromrus@gmail.com>
2021-10-06 23:34:44 +1000

    init

---
+++ src/blocks/modules/header-nav/header-nav.js
@@ -0,0 +1,13 @@
+modules.define('header-nav', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
